7b62894e4dc5fdd5a387ef0341edfca7a3319931
o-kc <kevinchen060208@gmail.com>
2026-01-08 22:13:46 -0500

    some bugfixes and tweaks to server, now it sends name along with chat and doesnt break as often

--- server.c
+++ server.c
@@ -7,7 +7,11 @@ int main(int argc, char * argv[]) {
   tv.tv_sec = 1;
   tv.tv_usec = 0; //controls length that select will block (not using NULL since server may need actions independent of client)
 
+  initialize_c();
+
+
   int listen_socket = makeServer();
+  maxfd = listen_socket;
 
   fd_set master_sds;
   FD_ZERO( & master_sds);
@@ -43,7 +47,7 @@ void recv_respond(int client_socket, fd_set * write_sds, fd_set * master_sds) {
 
   int bytes = recv(client_socket, chat, SIZE - 1, 0); //recieve
 
-  if (bytes < 0) {
+  if (bytes <= 0) {
     close(client_socket);
     delete_client(client_socket);
     FD_CLR(client_socket, master_sds);
@@ -52,9 +56,13 @@ void recv_respond(int client_socket, fd_set * write_sds, fd_set * master_sds) {
 
   chat[bytes] = '\0';
 
+  char name_chat[SIZE+60];
+  snprintf(name_chat, sizeof(name_chat), "%s: %s", get_cname(client_socket), chat);
+
+
   for (int fd = 0; fd <= maxfd; fd++) {
     if (FD_ISSET(fd, write_sds) && fd != client_socket) {
-      if (send(fd, chat, bytes, 0) < 0) { //and respond
+      if (send(fd, name_chat, strlen(name_chat)+1, 0) < 0) { //and respond
         close(fd);
         delete_client(fd);
         FD_CLR(fd, master_sds);
@@ -72,25 +80,29 @@ void listener(int listen_socket, fd_set * read_sds, fd_set * write_sds, fd_set *
     struct sockaddr_storage client_addr;
     int client_socket = server_tcp_handshake(listen_socket, & client_addr);
 
+    if (client_socket > maxfd) {
+      maxfd = client_socket;
+    }
+
     char client_ip[INET_ADDRSTRLEN];
     if (client_addr.ss_family == AF_INET) {
       struct sockaddr_in * s = (struct sockaddr_in * ) & client_addr;
-      inet_ntop(AF_INET, & s -> sin_addr, client_ip, sizeof(client_ip));
+      inet_ntop(AF_INET, & s -> sin_addr, client_ip, sizeof(client_ip));  //get IP
     } else {
       //idk IPV6?
     }
 
     char name[50];
-    int bytes = recv(client_socket, name, 49, 0);
+    int bytes = recv(client_socket, name, 49, 0);                         //get name
     name[bytes] = '\0';
-    add_client(client_socket, name, client_ip);
+    add_client(client_socket, name, client_ip);                           //add
 
     FD_SET(client_socket, master_sds);
     queue--;
   }
   for (int fd = 0; fd <= maxfd && queue > 0; fd++) {
-    if (FD_ISSET(fd, read_sds)) {
-      recv_respond(fd, write_sds, master_sds);
+    if (FD_ISSET(fd, read_sds) && fd != listen_socket) {
+      recv_respond(fd, write_sds, master_sds);                  //indicates server must recieve these client messages
       queue--;
     }
   }
@@ -105,15 +117,16 @@ int add_client(int fd, char * name, char * ip) {
       strncpy(clients[i].ip, ip, 16);
       clients[i].ip[INET_ADDRSTRLEN - 1] = '\0';
       clients[i].active = 1;
+      client_count++;
+      return i;
     }
-    return i;
   }
   return -1;
 }
 
 char * get_cname(int fd) {
   for (int i = 0; i < 100; i++) {
-    if (clients[i].fd == fd) {
+    if (clients[i].active && clients[i].fd == fd) {
       return clients[i].name;
     }
   }
@@ -122,7 +135,7 @@ char * get_cname(int fd) {
 
 char * get_cip(int fd) {
   for (int i = 0; i < 100; i++) {
-    if (clients[i].fd == fd) {
+    if (clients[i].active && clients[i].fd == fd) {
       return clients[i].ip;
     }
   }
@@ -133,6 +146,7 @@ void delete_client(int fd) {
   for (int i = 0; i < 100; i++) {
     if (clients[i].active && clients[i].fd == fd) {
       clients[i].active = 0;
+      client_count--;
       return;
     }
   }
