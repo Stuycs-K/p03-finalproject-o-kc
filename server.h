e4ddf897aa4f7339cd1fe66e1b6a14eb1bd9973a
o-kc <kevinchen060208@gmail.com>
2026-01-13 19:20:15 -0500

    deleted server.h and expermimenting with extern to fix bug issue

--- server.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-#include "server_helper.h"
-
-#endif
--- server_helper.h
+++ server_helper.h
@@ -5,7 +5,7 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 
-struct timeval tv;
+extern struct timeval tv;
 //EXTRA VARIABLES
 struct clientinfo{
   int fd;
@@ -13,54 +13,54 @@ struct clientinfo{
   char ip[INET_ADDRSTRLEN];
   int active;          //this makes adding and removing easier since theres no need to shift
 }; //1 active 0 inactive
-struct clientinfo clients[100];
-int client_count;             //tdlr: possible values to extract: fd, name, ip, count
+extern struct clientinfo clients[100];
+extern int client_count;             //tdlr: possible values to extract: fd, name, ip, count
 
-fd_set master_sds;
-fd_set read_sds;
-fd_set write_sds;
-int maxfd;
+extern fd_set master_sds;
+extern fd_set read_sds;
+extern fd_set write_sds;
+extern int maxfd;
 
-WINDOW * chat_win;
-WINDOW * input_win;
-int special_status;
+extern WINDOW * chat_win;
+extern WINDOW * input_win;
+extern int special_status;
 
 struct banned{
   char ip[INET_ADDRSTRLEN];
   int active;
 };
-struct banned blacklist[100];
+extern struct banned blacklist[100];
 
 
 
-void recv_respond(int client_socket);   //responds to an active client socket
-void whisper(char * name_chat, int cs, char * chat);
-void header(char* name_chat, int cs, char* chat, char*addon);  //formats a chat message to send based on the sent input
-void sender(int fd, int cs, char* name_chat);   //sends a chat message to a fd, with checks
+extern void recv_respond(int client_socket);   //responds to an active client socket
+extern void whisper(char * name_chat, int cs, char * chat);
+extern void header(char* name_chat, int cs, char* chat, char*addon);  //formats a chat message to send based on the sent input
+extern void sender(int fd, int cs, char* name_chat);   //sends a chat message to a fd, with checks
 
-void listener(int listen_socket);        //handles incoming fds
-int ip_convert_check(char* ip, struct sockaddr_storage client_addr);
-int recvname(int fd, char * name, char * ip);
+extern void listener(int listen_socket);        //handles incoming fds
+extern int ip_convert_check(char* ip, struct sockaddr_storage client_addr);
+extern int recvname(int fd, char * name, char * ip);
 
-int add_client(int fd, char*name, char* ip); //add a client struct to list
+extern int add_client(int fd, char*name, char* ip); //add a client struct to list
 
-char* get_cname(int fd);  //get name from fd input
-int get_cfd(char* name);  //get fd from name input
-char * get_cip(int fd);    //get ip from fd input
+extern char* get_cname(int fd);  //get name from fd input
+extern int get_cfd(char* name);  //get fd from name input
+extern char * get_cip(int fd);    //get ip from fd input
 
-void delete_client(int fd);               //removes a client struct from list
-void delete_client_name(char* key, int ban);  //removes based on name and add to b_list
-int new_maxfd(int old_max);  //updates the max_fd
+extern void delete_client(int fd);               //removes a client struct from list
+extern void delete_client_name(char* key, int ban);  //removes based on name and add to b_list
+extern int new_maxfd(int old_max);  //updates the max_fd
 
 
-int add_banned(char * ip);  //add a banned ip to list
-int is_banned(char * ip);   //checks if ip is banned
+extern int add_banned(char * ip);  //add a banned ip to list
+extern int is_banned(char * ip);   //checks if ip is banned
 
-void initialize_c();
-void initialize_b();
+extern void initialize_c();
+extern void initialize_b();
 
-void user_interface();
-void new_status(int mode, char * modname, int* pos, char*special_store);
-void status(char * modname, char*special_store);
-void parse_helper(int * pos, char * special_store, char * modname);
+extern void user_interface();
+extern void new_status(int mode, char * modname, int* pos, char*special_store);
+extern void status(char * modname, char*special_store);
+extern void parse_helper(int * pos, char * special_store, char * modname);
 #endif
